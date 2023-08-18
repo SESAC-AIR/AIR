0fe14e1ef144d3f7afb95c140e5e2fa7d19ab6b3
365saiom <saiom365@naver.com>
2023-08-18 12:58:16 +0900

    메인 애니메이션 수정, 역사카드 추가

--- js/three.js
+++ js/three.js
@@ -18,11 +18,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+
 const container = document.querySelector(".threejs-container");
 container.appendChild(renderer.domElement);
 
 // 빛
-const light = new THREE.DirectionalLight(0xffffff, 1);
+const light = new THREE.AmbientLight(0xffffff, 1.7);
 light.position.set(1, 1, 1);
 scene.add(light);
 
@@ -31,9 +32,9 @@ const GLTFloader = new GLTFLoader();
 const GLTFObjGroup = new THREE.Object3D();
 
 // load a resource
-GLTFloader.load("../nike_jordan/scene.gltf", function(gltf){
+GLTFloader.load("../air_jordan_1_1985/scene.gltf", function(gltf){
   const GLTFObj = gltf.scene
-  GLTFObj.scale.set(1, 1, 1)
+  GLTFObj.scale.set(0.005, 0.005, 0.005)
   GLTFObj.position.set(0, -0.1, 0)
   GLTFObjGroup.add(GLTFObj)
   scene.add(GLTFObjGroup)
@@ -59,34 +60,117 @@ ScrollTrigger.defaults({
 let modelAni = gsap.timeline();
 
 modelAni.to(camera.position, {
-  duration: 2,
-  y: -0.2,
-  z: 0.8, 
+  duration: 1,
+  x: 0,
+  y: -0.6,
+  z: 4, 
 })
 
+// 기준점
 modelAni.to(GLTFObjGroup.rotation,{
-  x: 12.5,
-  y: 0.1,
-  z: -0.01 ,
+  x: 0,
+  y: 0,
+  z: 0,
+  scrollTrigger: {
+    trigger:"header",
+    start: "top bottom",
+    end: "top bottom",
+    scrub: 1,
+    // markers: true,
+},
+})
+
+modelAni.to(camera.position,{
+  x: 0,
+  y: -0.6,
+  z: 3, 
   scrollTrigger: {
     trigger: ".section1",
-    start: "top top",
-    end: "bottom center",
-    markers: true
+    start: "top bottom",
+    end: "top bottom",
+    // markers: true,
   }
 })
 
-modelAni.to(camera.position, {
-  x: 0, 
-  y: 0.2,
-  z: -0.01,
+
+// content1 
+modelAni.to(GLTFObjGroup.rotation,{
+  x: 22,
+  y: 3,
+  z: -3.4,
+  scrollTrigger: {
+    trigger:"header",
+    start: "top top",
+    end: "bottom top",
+    scrub: 1,
+    // markers: true,
+}
+})
+
+modelAni.to(camera.position,{
+  x: 0,
+  y: -0.6,
+  z: 3, 
   scrollTrigger: {
     trigger: ".section1",
-    start: "top center",
-    end: "bottom bottom",
-    markers: true
+    start: "top bottom",
+    end: "top bottom",
+    // markers: true,
   }
 })
+
+// content2
+modelAni.to(GLTFObjGroup.rotation,{
+  x: 15.7,
+  y: -0.8,
+  z: -3.4,
+  scrollTrigger: {
+    trigger: ".section1",
+    start: "15% top",
+    end: "30% top",
+    // markers: true,
+  }
+})
+
+modelAni.to(camera.position,{
+  x: -1,
+  y: -0.6,
+  z: 3,
+  scrollTrigger: {
+    trigger: ".section1",
+    start: "15% top",
+    end: "30% top",
+    // markers: true,
+  }
+})
+
+// content3
+modelAni.to(GLTFObjGroup.rotation,{
+  x: 10.9,
+  y: 4,
+  z: -3.4,
+  scrollTrigger: {
+    trigger: ".section1",
+    start: "40% top",
+    end: "50% top",
+    markers: true,
+  }
+})
+
+modelAni.to(camera.position,{
+  x: 1,
+  y: -0.2,
+  z: 3,
+  scrollTrigger: {
+    trigger: ".section1",
+    start: "40% top",
+    end: "50% top",
+    markers: true,
+  }
+})
+
+
+
 
 
 // 애니메이션 함수
@@ -96,3 +180,5 @@ function animate(currentTime){
 }
 
 animate()
+
+
