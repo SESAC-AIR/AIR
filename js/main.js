ccf2e6f048216f63fb456d58868f693bea02dfd8
365saiom <saiom365@naver.com>
2023-08-19 17:28:55 +0900

    header 부분 통일

--- js/main.js
+++ js/main.js
@@ -40,38 +40,38 @@ const swiper2 = new Swiper(".swiper2", {
 //   }
 // });
 
-// search 모달창
+// // search 모달창
 
-const modalSearch = document.querySelector(".modal-search");
-const x = modalSearch.querySelector(".fa-xmark");
-const search = modalSearch.querySelector(".search");
-const field = search.querySelector(".field");
-const inputSearch = field.querySelector("input[type='search']");
-// console.log(inputSearch);
-const hashtag = modalSearch.querySelector("p");
-// const hashTag = field.querySelector(".hash-tag");
-inputSearch.addEventListener("focus", function () {
-  hashtag.style.display = "block";
-  // console.log(this);
-});
+// const modalSearch = document.querySelector(".modal-search");
+// const x = modalSearch.querySelector(".fa-xmark");
+// const search = modalSearch.querySelector(".search");
+// const field = search.querySelector(".field");
+// const inputSearch = field.querySelector("input[type='search']");
+// // console.log(inputSearch);
+// const hashtag = modalSearch.querySelector("p");
+// // const hashTag = field.querySelector(".hash-tag");
+// inputSearch.addEventListener("focus", function () {
+//   hashtag.style.display = "block";
+//   // console.log(this);
+// });
 
-inputSearch.addEventListener("focusout", function () {
-  hashtag.style.display = "none";
-  // console.log(this);
-});
+// inputSearch.addEventListener("focusout", function () {
+//   hashtag.style.display = "none";
+//   // console.log(this);
+// });
 
-const menu = document.querySelector(".menu");
-const menuIcon = menu.querySelectorAll("li");
-const menuSearch = menuIcon[3];
-// console.log(menuSearch);
+// const menu = document.querySelector(".menu");
+// const menuIcon = menu.querySelectorAll("li");
+// const menuSearch = menuIcon[3];
+// // console.log(menuSearch);
 
-menuSearch.addEventListener("click", () => {
-  modalSearch.classList.add("active");
-});
+// menuSearch.addEventListener("click", () => {
+//   modalSearch.classList.add("active");
+// });
 
-x.addEventListener("click", () => {
-  modalSearch.classList.remove("active");
-});
+// x.addEventListener("click", () => {
+//   modalSearch.classList.remove("active");
+// });
 
 // 애니메이션 추가
 const shoes = document.querySelector(".threejs-container");
