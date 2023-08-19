d952679777905abc03b02b19f800a83a26da2abe
yangux <yeonie05@gmail.com>
2023-08-19 19:03:03 +0900

    feat: collections 페이지네이션

--- js/collections.js
+++ js/collections.js
@@ -1,42 +1,74 @@
 // pagination
-function colPagination() {
-  const colSection = document.querySelectorAll(".colContainer");
-  const colTit = document.querySelector(".colTit h2 span");
-  const colIndicator = document.querySelector("#colIndicator");
-  const pageBtn = colIndicator.querySelectorAll("button");
-  const listContainer = colIndicator.querySelector(".list-container");
-  const list = listContainer.querySelector("ul");
-  const li = list.querySelectorAll("li");
-  let lastPage = Number(list.querySelector("li:last-child").innerText);
-  let thisPage = Number(list.querySelector("li.clicked").innerText);
-  let listPos = 0;
+const colSections = document.querySelectorAll(".colContainer");
+const colTit = document.querySelector(".colTit h2 span");
+const colIndicator = document.querySelector("#colIndicator");
+const pageBtn = colIndicator.querySelectorAll("button");
+const listContainer = colIndicator.querySelector(".list-container");
+const list = listContainer.querySelector("ul");
+const lis = list.querySelectorAll("li");
+let lastPage = Number(list.querySelector("li:last-child").innerText);
+let thisPage = Number(list.querySelector("li.clicked").innerText);
+let listWidth = listContainer.offsetWidth;
+let listView = listWidth / 40;
 
-  pageBtn.forEach((button) => {
-    button.addEventListener("click", function () {
-      li[thisPage - 1].classList.remove("clicked");
+// resize
+addEventListener("resize", () => {
+  thisPage = Number(list.querySelector("li.clicked").innerText);
+  listWidth = listContainer.offsetWidth;
+  listView = listWidth / 40;
+  list.style.transform = `translate(${
+    -listWidth * parseInt(thisPage / listView)
+  }px)`;
+  console.log(
+    thisPage,
+    listWidth,
+    listView,
+    -listWidth * parseInt(thisPage / listView)
+  );
+});
 
-      if (this.className === "btnLeft" && thisPage > 1) {
-        if (thisPage % 5 === 1) {
-          list.style.transform = `translate(${
-            -200 * (parseInt(thisPage / 5) - 1)
-          }px)`;
-        }
-        // 6 -> 40 * 0
-        // 11 -> 40 * 1
-        // 16 -> 40 * 2
-        thisPage--;
-      } else if (this.className === "btnRight" && thisPage < lastPage) {
-        if (thisPage % 5 === 0) {
-          list.style.transform = `translate(${-40 * thisPage}px)`;
-        }
-        thisPage++;
-      }
-      li[thisPage - 1].classList.add("clicked");
-      colTit.innerText = thisPage;
+lis.forEach((li) => {
+  li.addEventListener("click", function () {
+    lis[thisPage - 1].classList.remove("clicked");
+    this.classList.add("clicked");
+    thisPage = Number(this.firstChild.innerText);
+    colTit.innerText = thisPage;
+
+    colSections.forEach((col) => {
+      col.classList.add("hide");
     });
+    colSections[thisPage - 1].classList.remove("hide");
   });
-}
-colPagination();
+});
+
+pageBtn.forEach((button) => {
+  button.addEventListener("click", function () {
+    lis[thisPage - 1].classList.remove("clicked");
+
+    if (this.className === "btnLeft" && thisPage > 1) {
+      if (thisPage % listView === 1) {
+        list.style.transform = `translate(${
+          -listWidth * (parseInt(thisPage / listView) - 1)
+        }px)`;
+      }
+      thisPage--;
+    } else if (this.className === "btnRight" && thisPage < lastPage) {
+      if (thisPage % listView === 0) {
+        list.style.transform = `translate(${
+          (-listWidth * thisPage) / listView
+        }px)`;
+      }
+      thisPage++;
+    }
+    lis[thisPage - 1].classList.add("clicked");
+    colTit.innerText = thisPage;
+
+    colSections.forEach((col) => {
+      col.classList.add("hide");
+    });
+    colSections[thisPage - 1].classList.remove("hide");
+  });
+});
 
 // visual
 function colVisual() {
