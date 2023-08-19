383945e5e1999ca2c05d0584345c0cf00b259287
yangux <yeonie05@gmail.com>
2023-08-19 16:39:05 +0900

    feat: collections페이지 indicator 버튼 기능

--- js/collections.js
+++ js/collections.js
@@ -1,22 +1,45 @@
-const shoeToggle = document.querySelectorAll(".shoeToggle button");
-const lows = document.querySelectorAll(".shoe-box.low");
-const highs = document.querySelectorAll(".shoe-box.high");
+// pagination
+function colPagination() {
+  const colSection = document.querySelectorAll(".colContainer");
+  const colTit = document.querySelector(".colTit h2 span");
+  const colIndicator = document.querySelector("#colIndicator");
+  const pageBtn = colIndicator.querySelectorAll("button");
+  const listContainer = colIndicator.querySelector(".list-container");
+  const list = listContainer.querySelector("ul");
+  const li = list.querySelectorAll("li");
+  let lastPage = Number(list.querySelector("li:last-child").innerText);
+  let thisPage = Number(list.querySelector("li.clicked").innerText);
+  let listPos = 0;
 
-shoeToggle.forEach((button) => {
-  button.addEventListener("click", function () {
-    this.classList.toggle("checked");
-    this.querySelector("i").classList.toggle("fa-square-check");
-    this.querySelector("i").classList.toggle("fa-square");
+  pageBtn.forEach((button) => {
+    button.addEventListener("click", function () {
+      li[thisPage - 1].classList.remove("clicked");
 
-    if (this.id === "lowBtn") {
-      lows.forEach((shoeBox) => shoeBox.classList.toggle("hide"));
-    } else {
-      highs.forEach((shoeBox) => shoeBox.classList.toggle("hide"));
-    }
+      if (this.className === "btnLeft" && thisPage > 1) {
+        if (thisPage % 5 === 1) {
+          list.style.transform = `translate(${
+            -200 * (parseInt(thisPage / 5) - 1)
+          }px)`;
+        }
+        // 6 -> 40 * 0
+        // 11 -> 40 * 1
+        // 16 -> 40 * 2
+        thisPage--;
+      } else if (this.className === "btnRight" && thisPage < lastPage) {
+        if (thisPage % 5 === 0) {
+          list.style.transform = `translate(${-40 * thisPage}px)`;
+        }
+        thisPage++;
+      }
+      li[thisPage - 1].classList.add("clicked");
+      colTit.innerText = thisPage;
+    });
   });
-});
+}
+colPagination();
 
-function collectionVisual() {
+// visual
+function colVisual() {
   const visualImg = document.querySelector(
     ".colVisual .imgArea .imgWrap:nth-child(2)"
   );
@@ -26,4 +49,26 @@ function collectionVisual() {
     if (i > 4) i = 1;
   }, 1000);
 }
-collectionVisual();
+colVisual();
+
+// Products
+function shoeTypeToggle() {
+  const shoeToggleBtn = document.querySelectorAll(".shoeToggle button");
+  const lows = document.querySelectorAll(".shoe-box.low");
+  const highs = document.querySelectorAll(".shoe-box.high");
+
+  shoeToggleBtn.forEach((button) => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("checked");
+      this.querySelector("i").classList.toggle("fa-square-check");
+      this.querySelector("i").classList.toggle("fa-square");
+
+      if (this.id === "lowBtn") {
+        lows.forEach((shoeBox) => shoeBox.classList.toggle("hide"));
+      } else {
+        highs.forEach((shoeBox) => shoeBox.classList.toggle("hide"));
+      }
+    });
+  });
+}
+shoeTypeToggle();
